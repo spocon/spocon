16b45e72f5f078544f3bb9df586daa5d4f5d0901
Thorsten Haga <thorsten.haga@tradedoubler.com>
2019-07-17 03:46:31 +0200

    launchpad ppa upload done : https://launchpad.net/~spocon/+archive/ubuntu/spocon

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible=2.5.1+dfsg-1
+apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible=2.5.1+dfsg-1 gnupg-agent essential-build
