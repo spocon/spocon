6e98e14f47abe43faf91302c932da962551e0f13
Thorsten Haga <thorsten.haga@tradedoubler.com>
2019-07-18 13:32:49 +0200

    updated dependencies of the vagrant

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible=2.5.1+dfsg-1 gnupg-agent essential-build
+apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible=2.5.1+dfsg-1 gnupg-agent pinentry-tty debhelper devscripts apt-ftparchive
+cp -f /home/vagrant/workspace/Vagrant/gpg-agent.conf /home/vagrant/.gnupg & chown vagrant:vagrant /home/vagrant/.gnupg/gpg-agent.conf
+echo "Please add your ssh_key in ~/.ssh/id_rsa and your private gpg key passphrase to ~/.bashrc => echo mypassphrase | /usr/lib/gnupg2/gpg-preset-passphrase -c gpgkey"
