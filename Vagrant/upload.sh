68df71f7d4d136b237e569a1aae8dda10307928e
spocon <spocondeb@gmail.com>
2019-12-07 16:33:45 +0100

    improved upload script

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 export UPLOAD=true
-ansible-playbook Ansible/start.yml -e librespot_version=1.0.0 -e spocon_version=1.0.0
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.0.0 -e spocon_version=1.0.0
+
