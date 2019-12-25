e2bca0fd481dca59a909a5da3db9df7b9d127d95
spocon <spocondeb@gmail.com>
2019-12-25 18:53:47 +0100

    SPOCON added 1.1.0 and improved upload

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 export UPLOAD=true
-ansible-playbook ../Ansible/start.yml -e librespot_version=1.0.0 -e spocon_version=1.0.0
+GPG_TTY=$(tty)
+export GPG_TTY
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.1.0 -e spocon_version=1.1.0
 
