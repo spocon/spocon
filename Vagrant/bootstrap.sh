7fbd7150ec788d81c7ca076e1e197cc66e3e9e30
Thorsten Haga <thorsten.haga@tradedoubler.com>
2020-02-05 21:35:43 +0100

    SPOCON: added frontend tools in virtualbox

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -2,5 +2,6 @@
 
 apt-get update -y
 apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible gnupg-agent pinentry-tty debhelper devscripts apt-utils
+apt-get install -y lubuntu-desktop qt5-default qtcreator  #for frontend development
 cp -f /home/vagrant/workspace/Vagrant/gpg-agent.conf /home/vagrant/.gnupg & chown vagrant:vagrant /home/vagrant/.gnupg/gpg-agent.conf
 echo "Please add your ssh_key in ~/.ssh/id_rsa and your private gpg key passphrase to ~/.bashrc => echo mypassphrase | /usr/lib/gnupg2/gpg-preset-passphrase -c gpgkey"
