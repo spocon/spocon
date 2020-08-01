7c678232416ec8795bc9337fb909b4895c5197ba
Spocon <spocondeb@gmail.com>
2020-08-01 09:10:22 +0200

    SPOCON-2020-08-01 RELEASE 1.5.1

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -3,6 +3,4 @@
 export UPLOAD=true
 GPG_TTY=$(tty)
 export GPG_TTY
-ansible-playbook ../Ansible/start.yml -e librespot_version=1.5.0 -e spocon_version=1.5.0
-
-
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.5.1 -e spocon_version=1.5.1
