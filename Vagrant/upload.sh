9d241946b12a2d816392adcf5ce408e23a3a20de
Spocon <spocondeb@gmail.com>
2020-12-28 18:23:21 +0100

    SPOCON-2020-12-28 updated new librespot version to 1.5.3 happy new year

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -3,4 +3,4 @@
 export UPLOAD=true
 GPG_TTY=$(tty)
 export GPG_TTY
-ansible-playbook ../Ansible/start.yml -e librespot_version=1.5.2 -e spocon_version=1.5.2
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.5.3 -e spocon_version=1.5.3
