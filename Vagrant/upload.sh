39f8f86dfdb9ea0776cfcc6040bdb7e1d918de4d
Spocon <spocondeb@gmail.com>
2021-02-14 21:36:20 +0100

    SPOCON-2021-02-14 upgraded version from librespot

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -3,4 +3,4 @@
 export UPLOAD=true
 GPG_TTY=$(tty)
 export GPG_TTY
-ansible-playbook ../Ansible/start.yml -e librespot_version=1.5.4 -e spocon_version=1.5.4
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.5.5 -e spocon_version=1.5.5
