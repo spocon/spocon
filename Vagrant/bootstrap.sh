3a69a0355315ed2a9da6404791c192ed8c4e4372
Spocon <spocondeb@gmail.com>
2021-12-31 12:59:02 +0100

    SPOCON-2020-07-29 RELEASE 1.6.2

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt-get update -y
-apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible gnupg-agent pinentry-tty debhelper devscripts apt-utils
-cp -f /home/vagrant/workspace/Vagrant/gpg-agent.conf /home/vagrant/.gnupg & chown vagrant:vagrant /home/vagrant/.gnupg/gpg-agent.conf
+apt-get install -y wget nano git dos2unix openjdk-11-jre virtualenv ansible gnupg-agent pinentry-tty debhelper devscripts apt-utils docker.io
+mkdir -p /home/vagrant/.gnupg
+cp -f /home/vagrant/workspace/Vagrant/gpg-agent.conf /home/vagrant/.gnupg & chown -R vagrant:vagrant /home/vagrant/.gnupg/gpg-agent.conf
+usermod -a -G docker vagrant
 echo "Please add your ssh_key in ~/.ssh/id_rsa and your private gpg key passphrase to ~/.bashrc => echo mypassphrase | /usr/lib/gnupg2/gpg-preset-passphrase -c gpgkey"
--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -3,4 +3,4 @@
 export UPLOAD=true
 GPG_TTY=$(tty)
 export GPG_TTY
-ansible-playbook ../Ansible/start.yml -e librespot_version=1.6.1 -e spocon_version=1.6.1
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.6.2 -e spocon_version=1.6.2
