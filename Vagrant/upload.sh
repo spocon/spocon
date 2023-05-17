29e5182cd88c9babe53dce69300e6ebf7082d6f9
Alex <36776298+Variiuz@users.noreply.github.com>
2023-05-17 23:20:20 +0200

    Update upload.sh librespot and spocon bump to 1.6.3
    https://github.com/librespot-org/librespot-java/releases/tag/v1.6.3

--- Vagrant/upload.sh
+++ Vagrant/upload.sh
@@ -3,4 +3,4 @@
 export UPLOAD=true
 GPG_TTY=$(tty)
 export GPG_TTY
-ansible-playbook ../Ansible/start.yml -e librespot_version=1.6.2 -e spocon_version=1.6.2
+ansible-playbook ../Ansible/start.yml -e librespot_version=1.6.3 -e spocon_version=1.6.3
